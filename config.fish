d0d98e8fa3597cab9e0c7d4c687f85f61c6cf414
Son Dong Xuan <sondongxuan1205@gmail.com>
2025-03-09 12:08:44 +0700

    add fish config

---
+++ config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	alias vi nvim
+	alias ll "eza -la --icons"
+	alias tmn "tmux new -s"
+	alias tma "tmux attach -t"
+	alias tmd "tmux detach"
+  alias lg "lazygit"
+
+    nvm use lts
+end
